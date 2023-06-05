9b3fde681d06a79f4a7a3c439fb0763119156b18
Vladyslav Koponkin <vladyslav.k@collectius.com>
2023-06-05 17:21:27 +0100

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -7,64 +7,77 @@ import typescript from 'gulp-typescript';
 import { create } from 'browser-sync';
 import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+// import changed from 'gulp-changed';
+// import prettier from 'gulp-prettier';
+import autoprefixer from 'autoprefixer';
+
 
 const sass = gulpSass(dartSass);
 const browserSync = create();
 
 function browserSyncInit(done) {
-    browserSync.init({
-        server: {
-            baseDir: './dist',
-            index: 'index.html',
-        },
-        tunnel: true,
-        host: 'localhost',
-        port: 9000,
-        notify: false,
-    });
-    done();
+	browserSync.init({
+		server: {
+			baseDir: './dist',
+			index: 'index.html',
+		},
+		tunnel: true,
+		host: 'localhost',
+		port: 9000,
+		notify: false,
+	});
+	done();
 }
 
 function browserSyncReload(done) {
-    browserSync.reload();
-    done();
+	browserSync.reload();
+	done();
 }
 
 function clean() {
-    return deleteAsync(['./dist/']);
+	return deleteAsync(['./dist/']);
 }
-   
+
 
 // Compile Pug to HTML
-gulp.task('pug', function() {
-  return gulp.src('src/**/*.pug')
-    .pipe(pug())
-    .pipe(gulp.dest('dist'));
+gulp.task('pug', function () {
+	return gulp.src('src/pages/*.pug')
+		.pipe(pug({ pretty: true }))
+		.pipe(gulp.dest('./dist/'));
 });
 
 // Compile SCSS to CSS
-gulp.task('scss', function() {
-  return gulp.src('src/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist'));
+gulp.task('scss', function () {
+	const plugins = [autoprefixer({ grid: true })];
+	
+	return gulp
+		.src('src/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass())
+		.pipe(postcss(plugins))
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest('./dist/css'))
+		.pipe(browserSync.stream());
 });
 
 // Compile TypeScript to JavaScript
-gulp.task('typescript', function() {
-  return gulp.src('src/**/.ts')
-    .pipe(typescript())
-    .pipe(gulp.dest('dist'));
+gulp.task('typescript', function () {
+	return gulp.src('src/**/.ts')
+		.pipe(typescript())
+		.pipe(gulp.dest('./dist'));
 });
 
 // Watch for file changes
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.pug', gulp.series('pug'));
-  gulp.watch('src/**/*.scss', gulp.series('scss'));
-  gulp.watch('src/**/*.ts', gulp.series('typescript'));
+gulp.task('watch', function () {
+	gulp.watch('src/**/*.pug', gulp.series('pug', browserSyncReload));
+	gulp.watch('src/**/*.scss', gulp.series('scss', browserSyncReload));
+	gulp.watch('src/**/*.ts', gulp.series('typescript', browserSyncReload));
 });
 
 // Default task
 gulp.task(
-    'default',
-    gulp.series(clean, 'pug', 'scss', 'typescript', gulp.parallel('watch', browserSyncInit))
+	'default',
+	gulp.series(clean, 'pug', 'scss', 'typescript', gulp.parallel('watch', browserSyncInit))
 );
